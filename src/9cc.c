c6158badb4b3866946d0937ce0ff370336194d2f
hitsan <soledewa2828@gmail.com>
2021-02-20 02:08:52 +0900

    add parser

--- src/9cc.c
+++ src/9cc.c
@@ -5,12 +5,24 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
 typedef enum{
     TK_RESERVED,
     TK_NUM,
     TK_EOF,
 } TokenKind;
 
+typedef enum{
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_NUM,
+} NodeKind;
+
+
+typedef struct Node Node;
 typedef struct Token Token;
 
 struct Token{
@@ -20,8 +32,43 @@ struct Token{
     char *str;
 };
 
+struct Node{
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
 Token *token;
 char *user_input;
+
+// proto type
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+void error_at(char *loc, char *fmt, ...);
+void error(char *fmt, ...);
+bool consume(char op);
+void expect(char op);
+int expect_number();
+bool at_eof();
+Node *primary();
+Node *mul();
+Node *expr();
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+} 
+
+Node *new_node_num(int val){
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+} 
 
 void error_at(char *loc, char *fmt, ...){
     va_list ap;
@@ -79,6 +126,74 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
     return tok;
 }
 
+Node *primary(){
+    if(consume('(')){
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+
+    return new_node_num(expect_number());
+}
+
+Node *mul(){
+    Node *node = primary();
+
+    for(;;){
+        if(consume('*')){
+            node = new_node(ND_MUL, node, primary());
+        }else if(consume('/')){
+            node = new_node(ND_DIV, node, primary());
+        }else{
+            return node;
+        }
+    }
+}
+
+Node *expr(){
+    Node *node = mul();
+
+    for(;;){
+        if(consume('+')){
+            node = new_node(ND_ADD, node, mul());
+        }else if(consume('-')){
+            node = new_node(ND_SUB, node, mul());
+        }else{
+            return node;
+        }
+    }
+}
+
+void gen(Node *node){
+    if(node->kind == ND_NUM){
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch(node->kind){
+        case ND_ADD:
+            printf("  add rax, rdi\n");
+            break;
+        case ND_SUB:
+            printf("  sub rax, rdi\n");
+            break;
+        case ND_MUL:
+            printf("  imul rax, rdi\n");
+            break;
+        case ND_DIV:
+            printf("  cqo\n");
+            printf("  idiv rdi\n");
+            break;
+    }
+    printf("  push rax\n");
+}
+
 Token *tokenize(){
     char *p = user_input;
     Token head;
@@ -91,7 +206,7 @@ Token *tokenize(){
             continue;
         }
 
-        if(*p == '+' || *p == '-'){
+        if(*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')'){
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -111,30 +226,22 @@ Token *tokenize(){
 }
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    error("Illegal number of argument!\n");
-    return 1;
-  }
+    if (argc != 2) {
+      error("Illegal number of argument!\n");
+      return 1;
+    }
 
-  user_input = argv[1];
-  token = tokenize();
+    user_input = argv[1];
+    token = tokenize(user_input);
+    Node *node = expr();
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
-
-  while(!at_eof()){
-      if(consume('+')){
-          printf("  add rax, %d\n", expect_number());
-          continue;
-      }
-
-      expect('-');
-      printf("  sub rax, %d\n", expect_number());
-  }
-
-  printf("  ret\n");
-  return 0;
+    gen(node);
+    
+    printf("  pop rax\n");
+    printf("  ret\n");
+    return 0;
 }
